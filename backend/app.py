607f9d7fdc0839403d4d72a73dd8a1402145c7dc
SaFernandezC <SFernandezC@fi.uba.ar>
2023-06-23 18:39:37 -0300

    fix: update .gitignore

--- backend/app.py
+++ backend/app.py
@@ -15,9 +15,9 @@ db.create_password_table()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:4200"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
